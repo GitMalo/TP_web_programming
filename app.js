a36957a85e6d36888aa85e38adcdad7b61adee25
mbonnotte <malo.bonnotte@epfedu.fr>
2023-05-18 16:55:50 +0200

    tp3_final

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 app.set('views','./views');
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 // print the string
 app.get('/', (req, res) => {
@@ -16,12 +18,6 @@ app.get('/students', (req, res) => {
   res.render("students", {students: [{ name: "Malo Bonnotte", school:"EPF"}, { name: "Harry Potter", school: "Poudlard"}]})
 })
 
-
-// print student informations
-app.get('/api/students', (req, res) => {
-  res.send([{ name: "Malo Bonnotte", school: "EPF" }, { name: "Harry Potter", school: "Poudlard"}])
-
-})
 
 
 // print the csv file 
@@ -53,22 +49,48 @@ app.get("/students-csv-parse", (req, res) => {
   });
 });
 
-app.use(express.json())
 
-app.post("/api/students/create", (req, res) => {
-  console.log(req.body);
-  const csvLine =`\r\n${req.body.name};${req.body.school}`;
-  const stream = fs.writeFile(
-  "./web_programming_test.csv",
-    csvLine,{ flag: "a"},
-    (err) => {
-      if (err){
-        res.send(err)
-      } else{
-  res.send("Student created");
-  }
-  });
+app.get("/students/create", (req, res) => {
+  res.render("create-student");
 });
+
+const storeStudentInCsvFile = (student, cb) => {
+    const csvLine = `\n${student.name};${student.school}`;
+    console.log(csvLine);
+    fs.writeFile("./web_programming_test.csv", csvLine, { flag: "a" }, (err) => {
+      cb(err, "ok");
+    });
+  };
+
+app.post("/students/create", (req, res) => {
+    console.log(req.body);
+    const student = req.body;
+    storeStudentInCsvFile(student, (err, storeResult) => {
+      if (err) {
+        res.redirect("/students/create?error=1");
+      } else {
+        res.redirect("/students/create?created=1");
+      }
+    });
+  });
+  
+app.get("/api/students", (req, res) => {
+    getStudentsFromCsvfile((err, students) => {
+      res.send(students);
+    });
+  });
+  
+  app.post("/api/students/create", (req, res) => {
+    console.log(req.body);
+    const student = req.body;
+    storeStudentInCsvFile(student, (err, storeResult) => {
+      if (err) {
+        res.status(500).send("error");
+      } else {
+        res.send("ok");
+      }
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
