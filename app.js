91992f53b8286fe7f3f886e95f6f975812ef4727
mbonnotte <malo.bonnotte@epfedu.fr>
2023-05-19 13:58:34 +0200

    tp4_v1

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express')
 const port = 3000
 const fs = require("fs")
 const path = require("path");
+const app = require('express')()
+const basicAuth = require('express-basic-auth')
 
 app.set('views','./views');
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+app.use(basicAuth({
+    users: { 'admin': 'supersecret' },
+    challenge: true,
+}))
 
 // print the string
 app.get('/', (req, res) => {
